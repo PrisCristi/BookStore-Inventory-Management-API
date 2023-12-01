88dfc84bc477f3f53794af1bea729fe48e54fa19
PrisCristi <santos.priscila85@gmail.com>
2023-12-01 19:11:38 +0100

    Main class altereted

--- src/main/java/org/example/DBConnection.java
+++ src/main/java/org/example/DBConnection.java
@@ -4,36 +4,40 @@ import java.sql.*;
 
 public class DBConnection {
 
-    public static DBConnection getConnection() {
+    public static Connection getConnection() {
         String url = "jdbc:mysql://localhost:3306/bookstore";
         String user = "root";
         String password = "Berlin!!2023";
 
+        // Abrir conexao com DB e conectcar com ela
         try {
-            Connection con = null;
-            try {
-                con = DriverManager.getConnection(url, user, password);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            String query = "SELECT * FROM books WHERE title > ? AND author = ?";
-            PreparedStatement pst =  pst = con.prepareStatement(query);
-
-            pst.setString(1, "The Color Purple");
-            pst.setString(2, "Ferminism is for Everybody");
-            ResultSet myRs = pst.executeQuery();
-            System.out.println("Title\t Author");
-
-            while (myRs.next()) {
-                String name = myRs.getString("Author");
-                String title = myRs.getString("Title");
-                System.out.println(name + "\t " + title);
-            }
-            con.close();
-        } catch (Exception e) {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void closeConnection(Connection con) {
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
     }
 }
 
+       /*
+
+        String query = "SELECT * FROM books WHERE title > ? AND author = ?";
+        PreparedStatement pst =  pst = con.prepareStatement(query);
+
+        pst.setString(1, "The Color Purple");
+        pst.setString(2, "Ferminism is for Everybody");
+        ResultSet myRs = pst.executeQuery();
+        System.out.println("Title\t Author");
+
+        while (myRs.next()) {
+        String name = myRs.getString("Author");
+        String title = myRs.getString("Title");
+        System.out.println(name + "\t " + title);
+        }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,17 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+
 import java.net.URI;
-import sun.net.httpserver.DefaultHttpServerProvider;
 
 public class Main {
-    private static DefaultHttpServerProvider GrizzlyHttpServerFactory;
 
     public static void main(String[] args) {
         String BASE_URI = "http://localhost:8080/";
-        ResourceConfig resourceConfig = new ResourceConfig(BookResource.class);
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI),resourceConfig);
+        ResourceConfig resourceConfig = new ResourceConfig( BookResource.class);
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
         System.out.println("Server started at: " + BASE_URI);
-
 
 
     }
